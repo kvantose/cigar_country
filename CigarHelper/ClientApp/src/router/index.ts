fe09fcc2a07299064b6b92107b7072f2c0f8786e
Eugene Kostyukov <evgenii.kostiukov@gmail.com>
2025-01-04 17:50:44 +0300

    asddas

---
+++ CigarHelper/ClientApp/src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/AuthView.vue'),
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('@/views/AccountView.vue'),
+    },
+    {
+      path: '/humidor',
+      name: 'humidor',
+      component: () => import('@/views/HumidorView.vue'),
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+  ],
+});
+
+export default router;
